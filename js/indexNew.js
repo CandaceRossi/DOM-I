691d25978e1741ccc1485e9117d2312a4faac335
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-05 15:09:29 -0400

    cleared my old work and set up for re-do of project

--- js/indexNew.js
+++ js/indexNew.js
@@ -1,109 +1,47 @@
 const siteContent = {
-    "nav": {
-        "nav-item-1": "Services",
-        "nav-item-2": "Product",
-        "nav-item-3": "Vision",
-        "nav-item-4": "Features",
-        "nav-item-5": "About",
-        "nav-item-6": "Contact",
-        "img-src": "img/logo.png"
-    },
-    "cta": {
-        "h1": "DOM Is Awesome",
-        "button": "Get Started",
-        "img-src": "img/header-img.png"
-    },
-    "main-content": {
-        "features-h4": "Features",
-        "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-        "about-h4": "About",
-        "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-        "middle-img-src": "img/mid-page-accent.jpg",
-        "services-h4": "Services",
-        "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-        "product-h4": "Product",
-        "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-        "vision-h4": "Vision",
-        "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    },
-    "contact": {
-        "contact-h4": "Contact",
-        "address": "123 Way 456 Street Somewhere, USA",
-        "phone": "1 (888) 888-8888",
-        "email": "sales@greatidea.io",
-    },
-    "footer": {
-        "copyright": "Copyright Great Idea! 2018"
-    },
+  nav: {
+    "nav-item-1": "Services",
+    "nav-item-2": "Product",
+    "nav-item-3": "Vision",
+    "nav-item-4": "Features",
+    "nav-item-5": "About",
+    "nav-item-6": "Contact",
+    "img-src": "img/logo.png"
+  },
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
+    "img-src": "img/header-img.png"
+  },
+  "main-content": {
+    "features-h4": "Features",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4": "About",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "middle-img-src": "img/mid-page-accent.jpg",
+    "services-h4": "Services",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4": "Product",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4": "Vision",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+  },
+  contact: {
+    "contact-h4": "Contact",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io"
+  },
+  footer: {
+    copyright: "Copyright Great Idea! 2018"
+  }
 };
 
-//nav logo
-let logo = document.getElementById('logo-img');
-logo.setAttribute('src', siteContent['nav']['img-src']);
-
-//nav bar
-let nav = document.querySelectorAll('a');
-nav[0].textContent = siteContent['nav']['nav-item-1'];
-nav[1].textContent = siteContent['nav']['nav-item-2'];
-nav[2].textContent = siteContent['nav']['nav-item-3'];
-nav[3].textContent = siteContent['nav']['nav-item-4'];
-nav[4].textContent = siteContent['nav']['nav-item-5'];
-nav[5].textContent = siteContent['nav']['nav-item-6'];
-
-//green nav color
-for (i = 0; i < 6; i++) {
-    nav[i].style.color = 'green';
-};
-
-//appended tags
-let newTag = document.querySelector('nav');
-let newTag1 = document.createElement('a');
-let newTag2 = document.createElement('a');
-newTag1.textContent = 'More';
-newTag2.textContent = 'Mas';
-newTag.appendChild(newTag1);
-newTag.prepend(newTag2);
-
-newTag1.style.color = 'green';
-newTag2.style.color = 'green';
-
-
-//top content
-let ctaH = document.querySelector('.cta-text h1');
-ctaH.textContent = siteContent['cta']['h1'];
-
-let ctaButton = document.querySelector('.cta-text button');
-ctaButton.textContent = siteContent['cta']['button'];
-
-let ctaImg = document.getElementById('cta-img');
-ctaImg.setAttribute('src', siteContent['cta']['img-src']);
-
-//middle content
-
-let topmainContent = document.querySelectorAll('.text-content h4');
-topmainContent[0].textContent = siteContent['main-content']['features-h4'];
-topmainContent[1].textContent = siteContent['main-content']['about-h4'];
-topmainContent[2].textContent = siteContent['main-content']['services-h4'];
-topmainContent[3].textContent = siteContent['main-content']['product-h4'];
-topmainContent[4].textContent = siteContent['main-content']['vision-h4'];
-
-let mainContent = document.querySelectorAll('.text-content p');
-mainContent[0].textContent = siteContent['main-content']['features-content'];
-mainContent[1].textContent = siteContent['main-content']['about-content'];
-mainContent[2].textContent = siteContent['main-content']['services-content'];
-mainContent[3].textContent = siteContent['main-content']['product-content'];
-mainContent[4].textContent = siteContent['main-content']['vision-content'];
-
-let midImg = document.getElementById('middle-img');
-midImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
-
-let contact = document.querySelectorAll('.contact h4');
-contact[0].textContent = siteContent['contact']['contact-h4'];
-
-let contacts = document.querySelectorAll('.contact p');
-contacts[0].textContent = siteContent['contact']['address'];
-contacts[1].textContent = siteContent['contact']['phone'];
-contacts[2].textContent = siteContent['contact']['email'];
-
-let footer = document.querySelector('footer p');
-footer.textContent = siteContent['footer']['copyright'];
+//Example Nav Logo
+let logo = document.getElementById("logo-img");
+logo.setAttribute("src", siteContent["nav"]["img-src"]);
